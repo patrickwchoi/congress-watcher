1a1e97af6febb3971d78f8c39e5a7c59ab4ad265
Patrick Choi <ptrckchoi@gmail.com>
2023-07-17 14:50:33 -0700

    trying to add more to member voting history, but might need to restruture

--- src/components/members/MemberVotingHistory.tsx
+++ src/components/members/MemberVotingHistory.tsx
@@ -13,20 +13,34 @@ const MemberVoteHistory: React.FC<memberVoteHistoryProps> = ({memberVoteHistoryD
   const member_id = memberVoteHistoryData.results[0].member_id;
   const handleClick = async () => {
     offset+=20;
-    //api call
     const res = await fetch(
       `/api/members/getVoteHistory?member_id=${member_id}&offset=${offset}`
     );
     const newVoteData = await res.json();
     setVotes(newVoteData.results[0].votes);
-    console.log(votes);
+  }
+  if (!votes) {
+    <div>No Recent Votes</div>
   }
   return (
     <div>
       {votes && votes.map((vote) => (
-        <div key={vote.vote_uri} className="border">
-          <h3>{vote.bill.title}</h3>
-          <h4>break</h4>
+        <div key={vote.vote_uri} className="border p-4 my-1 ">
+          <div className='billheader flex flex-row'>
+            <h2>{vote.bill.title}</h2>
+          </div>
+
+          <p>description: {vote.description}</p>
+          <p>Vote position: {vote.position}  ({vote.date})</p>
+          <div className='vote-breakdown flex flex-col border border-2'>
+            <h3>Vote Breakdown:</h3>
+            <p>yes: {vote.total.yes}</p>
+            <p>no: {vote.total.no}</p>
+            <p>present: {vote.total.present}</p>
+            <p>not voting: {vote.total.not_voting}</p>
+            <p>result: {vote.result}</p>
+          </div>
+          <a href={`../../bill/${vote.bill.bill_id}`}>View Bill</a>
         </div>
       ))}
       <button onClick={handleClick}>Next</button>
