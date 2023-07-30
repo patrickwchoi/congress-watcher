459d41989c1371d8b964250ecadce1f63cf245c5
Patrick Choi <ptrckchoi@gmail.com>
2023-07-30 16:32:16 -0700

    added search function

--- src/components/members/SearchMemberBar.tsx
+++ src/components/members/SearchMemberBar.tsx
@@ -11,18 +11,29 @@ const SearchMemberBar: React.FC<SearchMemberBarProps> = ({allMembers}) => {
   const [filteredMembers, setFilteredMembers] = useState<MemberListItemInfo[]>([]);
 
   useEffect(() => {
-    setFilteredMembers(allMembers.filter(member => member.first_name.includes(searchTerm) || member.last_name.includes(searchTerm) || member.state.includes(searchTerm)));
+    if (searchTerm.length < 1) {
+      return;
+    }
+    const trimmedSearchTerm = searchTerm.trim()
+    setFilteredMembers(allMembers.filter(member => 
+      member.first_name.toLowerCase().includes(trimmedSearchTerm.toLowerCase()) || 
+      member.last_name.toLowerCase().includes(trimmedSearchTerm.toLowerCase())
+    ));
   }, [searchTerm]);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  };
   return(
-    <div>
-      <input type="text" placeholder='Search Member'/>
+    <div className='bg-red-100'>
+      <input type="text" placeholder='Search Member' value={searchTerm} onChange={handleChange}/>
       
       {filteredMembers.length>0 && (
         <div className='search-results'>
           <ul>
             {filteredMembers.map((member) => (
-              <li key={member.id}>
+              <li key={member.id + 'searchres'}>
                 {member.first_name} {member.last_name}
               </li>
             ))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,49 +6,68 @@ import ListOfMembers from "@/components/members/ListOfMembers";
 import { MemberListData } from '@/types/MemberTypes';
 import SearchMemberBar from "@/components/members/SearchMemberBar";
 interface HomePageProps {
-  memberListData: MemberListData;
+  senateData: MemberListData;
+  houseData: MemberListData;
 }
 
-const HomePage:React.FC<HomePageProps> = ({memberListData}) => {
+const HomePage:React.FC<HomePageProps> = ({senateData, houseData}) => {
   return (
     <div>
       <h1>home page</h1>
-      <ListOfMembers memberListData={memberListData}/>
-      {/* <SearchMemberBar allMembers={allMembers}/> */}
+      <SearchMemberBar allMembers={[...senateData.results[0].members, ...houseData.results[0].members]}/>
+      <ListOfMembers memberListData={houseData}/>
       <AllBills />
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   if (!process.env.PROPUBLICA_API_KEY) {
+//     throw new Error("PROPUBLICA_API_KEY must be defined");
+//   }
+
+//   const memberListRes = await fetch( //start with list of 117 House members
+//     `https://api.propublica.org/congress/v1/117/house/members.json`,
+//     {
+//       headers: { "X-API-Key": process.env.PROPUBLICA_API_KEY },
+//     },
+//   );
+//   const memberListData = await memberListRes.json();
+
+//   return {
+//     props: {
+//       memberListData,
+//     }
+//   }
+// }
+
+export const getStaticProps:GetStaticProps = async () =>  { //runs only during site buildup
   if (!process.env.PROPUBLICA_API_KEY) {
     throw new Error("PROPUBLICA_API_KEY must be defined");
   }
 
-  const memberListRes = await fetch( //start with list of 117 House members
+  const senateListRes = await fetch( //start with list of 117 House members
+    `https://api.propublica.org/congress/v1/117/senate/members.json`,
+    {
+      headers: { "X-API-Key": process.env.PROPUBLICA_API_KEY },
+    },
+  );
+  const senateData = await senateListRes.json();
+  const houseListRes = await fetch( //start with list of 117 House members
     `https://api.propublica.org/congress/v1/117/house/members.json`,
     {
       headers: { "X-API-Key": process.env.PROPUBLICA_API_KEY },
     },
   );
-  const memberListData = await memberListRes.json();
+  const houseData = await houseListRes.json();
 
   return {
     props: {
-      memberListData,
-    }
-  }
+      senateData,
+      houseData
+      // allMembers: [...senateData.results[0].members, ...houseData.results[0].members],
+    },
+  };
 }
-
-// export const getStaticProps:GetStaticProps = async () =>  { //runs only during site buildup
-//   const senateData = await fetch(`/api/members/getMembers?congress=117&chamber=senate`).then(res => res.json());
-//   const houseData = await fetch(`/api/members/getMembers?congress=117&chamber=house`).then(res => res.json());
-  
-//   return {
-//     props: {
-//       allMembers: [...senateData.results[0].members, ...houseData.results[0].members],
-//     },
-//   };
-// }
 
 export default HomePage;
