78e91205abf00dc80133a871dbea079a997111de
Patrick Choi <ptrckchoi@gmail.com>
2023-08-06 23:14:12 -0700

    cosponsor css

--- src/components/bills/Cosponsors.tsx
+++ src/components/bills/Cosponsors.tsx
@@ -1,6 +1,6 @@
-import { CosponsorsProps } from "@/types/BillTypes";
-import { useState } from "react";
-
+import { CosponsorsProps, CosponsorsInfo } from "@/types/BillTypes";
+import React, { useState } from "react";
+import {getState, getParty} from "@/utils/util";
 /**
  * @param {CosponsorsProps} props
  * @param props.cosponsors list of cosponsors
@@ -15,35 +15,58 @@ const Cosponsors: React.FC<CosponsorsProps> = ({ cosponsors }) => {
   if (cosponsors.length == 0) {
     return <div>No Cosponsors</div>;
   }
+
+  interface cosponsorItemProps {
+    cosponsor: CosponsorsInfo;
+  }
+
+  const cosponsorItem: React.FC<cosponsorItemProps> = ({cosponsor}) => (
+    <li key={cosponsor.cosponsor_id} className="grid grid-cols-3 gap-4 py-2">
+      <a href={`../member/${cosponsor.cosponsor_id}`} className="text-1xl font-bold text-blue-link">
+        {cosponsor.cosponsor_title} {cosponsor.name}
+      </a>
+      <p>{getState(cosponsor.cosponsor_state)}</p>
+      <p>{getParty(cosponsor.cosponsor_party)}</p>
+    </li>
+  );
+
   const allCosponsors = (
     <div>
-      <ul>
-        {cosponsors.map((cosponsor) => (
-          <li key={cosponsor.cosponsor_id}>
-            <a href={cosponsor.cosponsor_uri}>{cosponsor.name}</a>
-          </li>
-        ))}
+      {/* Grid column headers */}
+      <div className="grid grid-cols-3 gap-4 my-4 font-bold text-lg border-b-2 border-black">
+        <div>Title & Name</div>
+        <div>State</div>
+        <div>Party</div>
+      </div>
+      
+      {/* Grid list */}
+      <ul className="divide-y divide-gray-300 ">
+        {cosponsors.map((cosponsor) => cosponsorItem({cosponsor}))}
       </ul>
     </div>
   );
-  const firstFiveCosponsors = //first 5 or less cosponsors
-    (
-      <div>
-        <ul>
-          {cosponsors.slice(0, 5).map((cosponsor) => (
-            <li key={cosponsor.cosponsor_id}>
-              <a href={cosponsor.cosponsor_uri}>{cosponsor.name}</a>
-            </li>
-          ))}
-        </ul>
+
+  const firstFiveCosponsors = (
+    <div>
+      {/* Grid column headers */}
+      <div className="grid grid-cols-3 gap-4 my-4 font-bold text-lg border-b-2 border-black">
+        <div>Title & Name</div>
+        <div>State</div>
+        <div>Party</div>
       </div>
-    );
+
+      {/* Grid list */}
+      <ul className="divide-y divide-gray-300 ">
+        {cosponsors.slice(0, 5).map((cosponsor) => cosponsorItem({cosponsor}))}
+      </ul>
+    </div>
+  );
 
   return (
-    <div className="">
-      {cosponsors ? (
+    <div className="w-1/2 flex flex-col">
+      {cosponsors.length ? (
         <div>
-          <h1>Cosponsors ({cosponsors.length}):</h1>
+          <h2>Bill Cosponsors ({cosponsors.length}):</h2>
           {showAllCosponsors ? allCosponsors : firstFiveCosponsors}
           <button onClick={handleClick} className="bg-blue-100">
             {cosponsors.length > 5 &&
--- src/pages/bill/[bill_id]/index.tsx
+++ src/pages/bill/[bill_id]/index.tsx
@@ -1,12 +1,9 @@
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
 import type { GetServerSideProps } from "next";
 import { SpecificBillData, AmendmentsData, RelatedBillData, CosponsorsData } from "@/types/BillTypes";
 import BillPageInfo from "@/components/bills/BillPageInfo";
 import Cosponsors from "@/components/bills/Cosponsors";
 
 /**
- * 
  * @param bill_id - This is NOT just the bill number, this is billnum-congressnum 
  * @returns 
  */
@@ -18,50 +15,30 @@ interface BillPageProps {
 }
 const BillPage: React.FC<BillPageProps> = ({bill_id, billData, amendmentsData, cosponsorsData}) => {
 
-  const [isLoading, setIsLoading] = useState(true);
-  
-  console.log(cosponsorsData)
   return (
     <div>
       <h2>Bill Page</h2>
       <BillPageInfo bill={billData.results[0]} />
 
-      {amendmentsData.results[0].amendments.length != 0 ? (
-        <div>
-          <h1>Amendments</h1>
-          <ul>
-            {amendmentsData.results[0].amendments.map((amendment) => (
-              <li key={amendment.amendment_number}>
-                <a href={amendment.congressdotgov_url}>{amendment.title}</a>
-              </li>
-            ))}
-          </ul>
+      <div className="bill-bottom flex flex-row items-center">
+        <div className="amendments-container w-1/2">
+          {amendmentsData.results[0].amendments.length != 0 ? (
+            <div className="amendments">
+              <h1>Amendments</h1>
+              <ul>
+                {amendmentsData.results[0].amendments.map((amendment) => (
+                  <li key={amendment.amendment_number}>
+                    <a href={amendment.congressdotgov_url}>{amendment.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <h1>No amendments</h1>
+          )}
         </div>
-      ) : (
-        <h1>No amendments</h1>
-      )}
-
-      {/* {relatedBillsData && relatedBillsData.results.related_bills ? (
-        <div>
-          <h1>Related Bills</h1>
-          <ul>
-            {relatedBillsData.results.related_bills.map((bill) => (
-              <li key={bill.bill_id}>
-                <a>{bill.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <h1>No related bills</h1>
-      )} */}
-
-      {/* TODO: make button so it shows max 5 cosponsors unless button is pressed */}
-      {cosponsorsData.results[0].cosponsors.length > 0 ? (
         <Cosponsors cosponsors={cosponsorsData.results[0].cosponsors} />
-      ) : (
-        <h1>No cosponsors</h1>
-      )}
+      </div>
     </div>
   );
 };
