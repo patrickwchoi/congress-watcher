8276a3058dace4602c3e0eabb82dbb14e99fd888
Patrick Choi <ptrckchoi@gmail.com>
2023-08-17 14:14:23 -0700

    added rough function to calculate what time rep was in office

--- src/components/members/MemberBio.tsx
+++ src/components/members/MemberBio.tsx
@@ -17,40 +17,40 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
   const handleFacebook = () => {
     window.open(memberInfo.facebook_account, "_blank");
   }
-  function getMembershipSummary(data: MemberData): string[] {
-    let summaries: string[] = [];
-
-    for (let role of data.roles) {
-        const { congress, chamber, state, district, start_date, end_date } = role;
-        const fullStateName = getState(state);  // Using the getState() function
-        const startYear = start_date.split('-')[0];
-        const endYear = end_date.split('-')[0] === "9999" ? "Present" : end_date.split('-')[0];
-        const districtInfo = district ? `, District ${district}` : "";
-
-        const existingSummaryIndex = summaries.findIndex(s => s.startsWith(`${chamber}\t${fullStateName}${districtInfo}`));
-
-        if (existingSummaryIndex > -1) {
-            const existingSummary = summaries[existingSummaryIndex];
-            const parts = existingSummary.split(" ");
-            parts[parts.length - 1] = `(${startYear}-${endYear})`;
-            summaries[existingSummaryIndex] = parts.join(" ");
-        } else {
-            const formattedSummary = `${chamber}\t${fullStateName}${districtInfo} ${congress} (${startYear}-${endYear})`;
-            summaries.push(formattedSummary);
-        }
+  const getMembershipSummary = (memberInfo: SpecificMemberInfo) => {
+    let ans = [];
+    let recentStartYear = "";
+    let recentChamber = "";
+    let oldEndDate = "";
+  
+    for (let role of memberInfo.roles) {
+      const { congress, chamber, state, district, start_date, end_date } = role;
+      const fullStateName = getState(state);
+      const startYear = start_date ? start_date.split('-')[0] : '';
+      const endYear = end_date ? (end_date.split('-')[0] === "9999" ? "Present" : end_date.split('-')[0]) : '';
+  
+      if (role.chamber === recentChamber) {
+        ans.pop();
+        ans.push(<h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({recentStartYear}-{oldEndDate})</h4>);
+      } else {
+        recentStartYear = startYear;
+        recentChamber = chamber;
+        oldEndDate = endYear;
+        
+        const line = chamber === "House" ? 
+          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName}, District {district} ({startYear}-{endYear})</h4> :
+          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({startYear}-{endYear})</h4>;
+  
+        ans.push(line);
+      }
     }
-
-    return summaries;
+  
+    return (
+      <div>
+        {ans}
+      </div>
+    );
   }
-
-  // const state = (acronym:string) => { //return full state name
-  //   const stateNames: {[key:string] : string} = {'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland', 'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York', 'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'};
-  //   if (stateNames[acronym]){
-  //     return stateNames[acronym];
-  //   }
-  //   return acronym;
-  // }
-
 
   const inOffice = () => {
     if (memberInfo.in_office){
@@ -66,6 +66,7 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
         <div className="header-section text-center">
           <h1>{roles.short_title} {memberInfo.first_name} {memberInfo.last_name}</h1>
         </div>
+            <h4>{getMembershipSummary(memberInfo)}</h4>
         <div className="info-section sm:grid sm:grid-cols-2 gap-4">
           <div>
             <h4><strong>Party: </strong>{getParty(memberInfo.current_party)}</h4>
@@ -76,7 +77,6 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
           <div>
             <h4><strong>In office: </strong>{inOffice()}</h4>
             <h4><strong>Number of terms: </strong>{memberInfo.roles.length}</h4>
-            <h4>{getMembershipSummary(memberInfo)}</h4>
           </div>
         </div>
         <div className="website-link">
