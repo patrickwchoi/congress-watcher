fbf82e29af8ec3cc20979eeddbb64ea29f562073
Patrick Choi <ptrckchoi@gmail.com>
2023-08-17 14:49:58 -0700

    fixed mistake in member year function and added css

--- src/components/members/MemberBio.tsx
+++ src/components/members/MemberBio.tsx
@@ -31,15 +31,15 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
       const endYear = end_date ? (end_date.split('-')[0] >= "2024" ? "Present" : end_date.split('-')[0]) : '';
       if (role.chamber === recentChamber) {
         ans.pop();
-        ans.push(<h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({recentStartYear}-{oldEndDate})</h4>);
+        ans.push(<h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({startYear}-{oldEndDate})</h4>);
       } else {
         recentStartYear = startYear;
         recentChamber = chamber;
         oldEndDate = endYear;
         
         const line = chamber === "House" ? 
-          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName}, District {district} ({startYear}-{endYear})</h4> :
-          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({startYear}-{endYear})</h4>;
+          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName}, District {district} ({recentStartYear}-{endYear})</h4> :
+          <h4 key={role.congress}><strong>{chamber}</strong>: {fullStateName} ({recentStartYear}-{endYear})</h4>;
         ans.push(line);
       }
     }
@@ -51,22 +51,20 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
   }
 
   return (
-    <div className="flex flex-col sm:flex-row items-center w-4/5 border bg-primary-dark border-black border-2 rounded-sm shadow-xl px-1 p-4 sm:px-4 justify-around">
+    <div className="flex flex-col sm:flex-row w-4/5 border bg-primary-dark border-black border-2 rounded-sm shadow-xl px-1 p-4 sm:px-4 justify-around">
       <Image src={portraitUrl} alt="member image" width={220} height={300} />
-      <div className="memberbio-text flex flex-col w-4/5 sm:w-1/2">
-        <div className="header-section text-center">
+      <div className="memberbio-text items-center flex flex-col w-4/5 sm:w-1/2">
+        <div className="header-section text-center mb-2">
           <h1>{roles.short_title} {memberInfo.first_name} {memberInfo.last_name}</h1>
         </div>
             <h4>{getMembershipSummary(memberInfo)}</h4>
         <div className="info-section sm:grid sm:grid-cols-2 gap-4">
           <div>
             <h4><strong>Party: </strong>{getParty(memberInfo.current_party)}</h4>
-            <h4><strong>State: </strong>{getState(roles.state)}</h4>
             <h4><strong>Age: </strong>{(getAge(memberInfo.date_of_birth))}</h4>
-            <h4><strong>Next election: </strong>{roles.next_election}</h4>
           </div>
           <div>
-            <h4><strong>Number of terms: </strong>{memberInfo.roles.length}</h4>
+            <h4><strong>Next election: </strong>{roles.next_election}</h4>
           </div>
         </div>
         <div className="website-link">
