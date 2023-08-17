084ef37ac3812a6ebd4f9191882bcdd08399a2ff
Patrick Choi <ptrckchoi@gmail.com>
2023-08-17 15:33:27 -0700

    members page looks nicer now

--- src/components/members/MemberBio.tsx
+++ src/components/members/MemberBio.tsx
@@ -51,7 +51,7 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
   }
 
   return (
-    <div className="flex flex-col sm:flex-row w-4/5 bg-primary-dark border-black border-2 rounded-sm shadow-xl px-1 p-4 sm:px-4 justify-around">
+    <div className="flex flex-col sm:flex-row bg-primary-dark border-black border-2 rounded-sm shadow-xl px-1 p-4 sm:px-4 justify-around">
       <Image src={portraitUrl} alt="member image" width={220} height={300} />
       <div className="memberbio-text items-center flex flex-col w-4/5 sm:w-1/2 justify-between">
         <div className="">
@@ -61,17 +61,20 @@ const MemberBio: React.FC<MemberBioProps> = ({ memberInfo, portraitUrl }) => {
               {getMembershipSummary(memberInfo)}
           <div className="info-section sm:grid sm:grid-cols-2 gap-4">
             <div>
-              <h4><strong>Party: </strong>{getParty(memberInfo.current_party)}</h4>
-              <h4><strong>Age: </strong>{(getAge(memberInfo.date_of_birth))}</h4>
+              <p><strong>Party: </strong>{getParty(memberInfo.current_party)}</p>
+              <p><strong>Age: </strong>{(getAge(memberInfo.date_of_birth))}</p>
+              <p><strong>Next election: </strong>{roles.next_election}</p>
             </div>
             <div>
-              <h4><strong>Next election: </strong>{roles.next_election}</h4>
+              <p><strong>Phone:</strong> {memberInfo.roles[0].phone}</p>
+              <p><strong>Address:</strong> {memberInfo.roles[0].office}</p>
             </div>
           </div>
         </div>
         <div className="website-link">
-          <a href={memberInfo.url} target="_blank" rel="noopener noreferrer">
+          <a className="flex flex-row" href={memberInfo.url} target="_blank" rel="noopener noreferrer">
             View Official Website
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6.25 5C5.56 5 5 5.56 5 6.25v11.5c0 .69.56 1.25 1.25 1.25h11.5c.69 0 1.25-.56 1.25-1.25V14a1 1 0 1 1 2 0v3.75A3.25 3.25 0 0 1 17.75 21H6.25A3.25 3.25 0 0 1 3 17.75V6.25A3.25 3.25 0 0 1 6.25 3H10a1 1 0 1 1 0 2H6.25ZM14 5a1 1 0 1 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0V6.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L17.586 5H14Z"/></svg>
           </a>
         </div>
       </div>
