6b893b712a3970b2628db35e31721cbef4d40d78
Patrick Choi <ptrckchoi@gmail.com>
2023-08-23 14:16:52 -0700

    added analytucs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from "next/app";
 import '@/styles/global.css';
 import Header from '@/components/Header';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // return <Component {...pageProps} />;
   return (
     <div className="flex flex-col items-center bg-primary min-h-screen">
       <Header />  
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
